c487ba27a3c9b561ee8af781c9f39a8215e06149
ValdemirJunior2020 <your-infojr.83@gmail.com>
2025-07-10 15:42:00 -0400

    second

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,28 +14,42 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark px-4">
-      <Link className="navbar-brand" to="/">QA Training</Link>
-      <div className="collapse navbar-collapse">
-        <ul className="navbar-nav ms-auto">
-          <li className="nav-item">
-            <Link className="nav-link" to="/">Start</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/leaderboard">Leaderboard</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/training">Training Guide</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/login">Admin</Link>
-          </li>
-          <li className="nav-item">
-            <button className="btn btn-outline-light btn-sm ms-3" onClick={handleLogout}>
-              Logout
-            </button>
-          </li>
-        </ul>
+    <nav className="navbar navbar-expand-lg" style={{ backgroundColor: "#003366" }}>
+      <div className="container-fluid">
+        <Link className="navbar-brand text-white" to="/">QA Training</Link>
+        <button
+          className="navbar-toggler bg-white"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <Link className="nav-link text-white" to="/">Start</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link text-white" to="/leaderboard">Leaderboard</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link text-white" to="/training">Training Guide</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link text-white" to="/login">Admin</Link>
+            </li>
+            <li className="nav-item">
+              <button className="btn btn-outline-light btn-sm ms-2" onClick={handleLogout}>
+                Logout
+              </button>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   );
